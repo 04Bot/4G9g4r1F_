90c3b58f5c3027e5203a7143150c8fa5db0b7289
04Bot <128223170+04Bot@users.noreply.github.com>
2024-11-13 15:20:21 +0100

    Update 4vedGF98_.lua

--- 4vedGF98_.lua
+++ 4vedGF98_.lua
@@ -1,7 +1,7 @@
 local screen = Instance.new("ScreenGui")
 screen.ResetOnSpawn = false
 screen.AutoLocalize = false
-screen.Parent = game.CoreGui
+screen.Parent = game.Players.LocalPlayer.PlayerGui
 
 local close_openGUI = Instance.new("TextButton")
 close_openGUI.BackgroundTransparency = 0.5
